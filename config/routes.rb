93534a030c5dadba0a6238c3c4ba778c325952e8
Wan Cheul Kim <wckim88@gmail.com>
2018-02-11 22:52:59 -0800

    Checkpoint-7: Commented area revised

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   resources :wikis
   resources :charges, only: [:new, :create]
 
+  devise_scope :user do
+    put 'users/downgrade' => 'users/registrations#downgrade'
+  end
+
   devise_for :users
 
   get 'about' => 'welcome#about'
+
+  get 'downgrade' => 'welcome#downgrade'
 
   root 'welcome#index'
 
